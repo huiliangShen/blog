cd22074926b143f067b040a31e3f679889bd5efc
banruo <291233404@qq.com>
2020-03-03 15:48:18 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://${test}@${url} master:${branch}
+git push -f https://${test}@${ref} master:${branch}
 
 cd -
