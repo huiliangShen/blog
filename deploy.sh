1061cb433727bbf9c521c7f45954502ecb31b15f
banruo <291233404@qq.com>
2020-03-03 14:57:18 +0800

    test

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn docs:build #npm run docs:build
+
+# 进入生成的文件夹
+cd my-blog/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://${test}@github.com/huiliangShen/blog.git master:gh-pages
